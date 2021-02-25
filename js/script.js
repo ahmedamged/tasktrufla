28eb25a465c6a8048b29bc561c1470d56c88c724
ahmedamged <ahmedamged@live.com>
2021-02-25 20:44:17 +0200

    Finishing project - adding mobile nav

--- js/script.js
+++ js/script.js
@@ -1,11 +1,29 @@
 $(document).ready(function(){
 
   $(window).on("resize", function(e) {
+      if(!$('.navbar').hasClass('d-none')){
+        $('.navbar').addClass('d-none');
+        $('.quote-btn').removeClass('d-none');
+        $('.phone-icon').removeClass('d-none');
+      }
       if ($(window).width() <= 768){
         $('.quote-btn').html("QUOTE");
+        $('.navbar-item:first-child .navbar-link').addClass('dropdown-toggle');
       }
       else{
         $('.quote-btn').html("GET A QUOTE");
       }
     }).trigger('resize');
+
+    $('#navbarDropdown').on('click', function functionName() {
+      $('.products-links').toggleClass('d-none');
+    });
+
+    $('#hamburger').on('click', function functionName() {
+      $('.navbar').toggleClass('d-none');
+      if ($(window).width() <= 768){
+        $('.quote-btn').toggleClass('d-none');
+        $('.phone-icon').toggleClass('d-none');
+      }
+    });
 });
