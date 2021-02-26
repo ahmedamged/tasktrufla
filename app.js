413ebbe97f9d90f4e7858d8fe2abe7258645940c
ahmedamged <ahmedamged@live.com>
2021-02-26 13:06:12 +0200

    Procfile for heroku deploying

--- app.js
+++ app.js
@@ -13,6 +13,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'))
 });
 
-app.listen(5000, () => {
-  console.log('Listening on port ' + 5000);
+app.listen(process.env.PORT, () => {
+  console.log('Listening on port ' + process.env.PORT);
 });
